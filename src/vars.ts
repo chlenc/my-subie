3eea806f50e6e49fb26581e68336d9f77cefaab3
Alexey Nagorny <anagorny@wavesplatform.com>
2020-03-03 16:50:10 +0300

    delete vars

--- src/vars.ts
+++
@@ -1,4 +0,0 @@
-export const shortDataUrl =
-  "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
-export const longDataUrl =
-  "http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}";

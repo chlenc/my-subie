1b48fb41b94d54e0834c9a63e53491a0c0281ced
Alexey Nagorny <anagorny@wavesplatform.com>
2020-03-25 15:31:21 +0300

    fix

--- src/vars.ts
+++ src/vars.ts
@@ -9,7 +9,7 @@ export const gens = [
     '2GEN (BG/BD/BK) 1993-1999',
     '3GEN (BH/BE) 1999-2004',
     '4GEN (BP/BL) 2003-2009',
-    'OTHER LEGACY & OB)',
+    'OTHER LEGACY & OB',
     'MEANEYE (GC/GF/GM) 1992-2000',
     'BUGEYE (GD/GG) 2000-2004',
     'BLOBEYE (GD/GG) 2003-2005',
